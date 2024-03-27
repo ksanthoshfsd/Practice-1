e21a95d599354b28b6ad59324227f60e622642f1
ksanthoshfsd <ksanthoshfsd@gmail.com>
2024-03-27 23:44:59 +0530

    Practice 1

---
+++ script.js
@@ -0,0 +1,12 @@
+//! CLASS PRACTICE
+
+class Resume {
+    constructor(Name,EduQal,Skills,Univ){
+    this.Name = Name;
+    this.EduQal= EduQal;
+    this.Skills = Skills;
+    this.Univ= Univ
+}
+}
+const Objec = new Resume ("Sandy","B.Tech",["MERN Full Stack Developer", "JAVA SCRIPT", "HTML","CSS"],"Dr.MGR University")
+console.log(Objec)
